7ab91eddfba4dbb4ebf8ee77612c2c81234846ca
Lovmy <lovmy@wanadoo.fr>
2024-07-25 09:55:36 +0200

    Script de compilation

---
+++ go.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo application ____________________________________________________________________________________________________________
+cd application
+cargo clean
+cargo build
+cd ..
+
+echo web_assembly ___________________________________________________________________________________________________________
+cd web_assembly
+cargo clean
+#cargo build --target wasm32-unknown-unknown --release
+wasm-pack build
+cd ..
