94859c5ba75d469643246467cfa67e3cbc63530b
BANASZAK Serge <serge.banaszak@eiffage.com>
2025-03-11 16:46:27 +0100

    Service Worker PWA

--- application/www/html/0/js/worker/service_worker.js
+++ application/www/html/0/js/worker/service_worker.js
@@ -1,4 +1,4 @@
-const VERSION = "0.0.1a";
+const VERSION = "0.0.1d";
 const FICHIERS_EN_CACHE = [
 	"main.js",
 	"web_worker.js",
@@ -13,8 +13,6 @@ const FICHIERS_EN_CACHE = [
 	"../../css/popup.css",
 	"../../css/style.css",
 	"../../css/tableau.css",
-	"../../images/192.png",
-	"../../images/512.png",
 	"../../langue/fr/index.html",
 	"../../wasm/web_assembly.wasm",
 	"../../../../favicon.ico",
@@ -22,32 +20,32 @@ const FICHIERS_EN_CACHE = [
 ];
 
 self.addEventListener("install", event => {
-	console.log( "[install] Service Worker installing.");
+	console.log( "[install] "+VERSION );
 	event.waitUntil ( caches.open( VERSION ).then( cache => cache.addAll( FICHIERS_EN_CACHE ) ) );
 });
-
+								
 self.addEventListener("activate", event => {
-	console.log("[activate] Service Worker activating.");
+	console.log( "[activate] "+VERSION );
 	// Suppression du cache obsolete
 	event.waitUntil ( caches
 		.keys()
 		.then(keys => keys.filter(key => key !== VERSION))
 		.then(keys =>
 		Promise.all ( keys.map(key => {
-			console.log(`[activate] Deleting cache ${key}`);
+			console.log( `[activate] Deleting cache ${key}` );
 			return caches.delete(key);
 		}))
 	));
 });
 
 self.addEventListener("fetch", event => {
-	console.log("[fetch] ");
+	console.log( "[fetch] "+event.request.url );
 	// Stratégie Cache-First
 	event.respondWith( caches
-		.match( event.request)								// On vérifie si la requête a déjà été mise en cache
-		.then( cached => cached || fetch(event.request))	// sinon on requête le réseau
-		.then( response => cache(event.request, response)	// on met à jour le cache
-		.then(() => response)								// et on résout la promesse avec l'objet Response
+		.match( event.request.url )								// On vérifie si la requête a déjà été mise en cache
+		.then( cached => cached || fetch( event.request.url ))	// sinon on requête le réseau
+		.then( response => cache( event.request.url, response )	// on met à jour le cache
+		.then(() => response)									// et on résout la promesse avec l'objet Response
 	));
 });
 
