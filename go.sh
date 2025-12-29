56982f9ef3e3847254e393da6dd8b3a1c508fe6b
Serge <serge@localhost.my.domain>
2025-12-29 10:37:04 +0100

    Configuration Alpine

--- go.sh
+++ go.sh
@@ -1,24 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
 # Pour le statique :
-#	rustup target add x86_64-unknown-linux-musl
-#	sudo apt-get install -y musl-tools
-#	cargo install cargo-vendor-openssl (si erreur utiliser plutot : openssl = { version = "*", features = ["vendored"] })
+#       rustup target add x86_64-unknown-linux-musl
+#       sudo apt-get install -y musl-tools
+#       cargo install cargo-vendor-openssl (si erreur utiliser plutot : openssl = { version = "*", features = ["vendored"] })
 
 # Pour le NAS :
-#	rustup target add aarch64-unknown-linux-gnu
-#	sudo apt-get install -y gcc-aarch64-linux-gnu pkg-config
+#       rustup target add aarch64-unknown-linux-gnu
+#       sudo apt-get install -y gcc-aarch64-linux-gnu pkg-config
 
 echo Mise a jour ____________________________________________________________________________________________________________
-#cd ~/.cargo/bin
-#./rustup update
+cd ~/.cargo/bin
+./rustup update
 cargo install wasm-pack
 
 echo application ____________________________________________________________________________________________________________
 cd ~/workspace/Rust/application
 cargo clean
-OPENSSL_STATIC=1 PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target x86_64-unknown-linux-musl
-#CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc cargo build --target aarch64-unknown-linux-gnu
+# OPENSSL_STATIC=1 PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target x86_64-unknown-linux-musl                    # Statique
+# CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc cargo build --target aarch64-unknown-linux-gnu    # NAS
+cargo build --release                                                                                                   # Alpine
 cd ..
 
 echo web_assembly ___________________________________________________________________________________________________________
