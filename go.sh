d1bf1a31a04e4353629446a010950c870df59c14
Serge <serge@localhost.my.domain>
2025-12-30 17:15:26 +0100

    Windows

--- go.sh
+++ go.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 # Pour le statique :
-#       rustup target add x86_64-unknown-linux-musl
-#       sudo apt-get install -y musl-tools
-#       cargo install cargo-vendor-openssl (si erreur utiliser plutot : openssl = { version = "*", features = ["vendored"] })
+# 	rustup target add x86_64-unknown-linux-musl
+# 	sudo apt-get install -y musl-tools
+# 	cargo install cargo-vendor-openssl (si erreur utiliser plutot : openssl = { version = "*", features = ["vendored"] })
 
 # Pour le NAS :
-#       rustup target add aarch64-unknown-linux-gnu
-#       sudo apt-get install -y gcc-aarch64-linux-gnu pkg-config
+# 	rustup target add aarch64-unknown-linux-gnu
+# 	sudo apt-get install -y gcc-aarch64-linux-gnu pkg-config
+
+# Pour Windows ( à partir d'un Linux )
+#	rustup target add x86_64-pc-windows-msvcrustup target add x86_64-pc-windows-msvc
+#	sudo apt-get install mingw-w64
 
 echo Mise a jour ____________________________________________________________________________________________________________
 cd ~/.cargo/bin
@@ -17,9 +21,9 @@ cargo install wasm-pack
 echo application ____________________________________________________________________________________________________________
 cd ~/workspace/Rust/application
 cargo clean
-# OPENSSL_STATIC=1 PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target x86_64-unknown-linux-musl                    # Statique
-# CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc cargo build --target aarch64-unknown-linux-gnu    # NAS
-cargo build --release                                                                                                   # Alpine
+# OPENSSL_STATIC=1 PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target x86_64-unknown-linux-musl			# Statique
+# CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc cargo build --target aarch64-unknown-linux-gnu	# NAS
+cargo build --release # --target x86_64-pc-windows-msvc									# Windows
 cd ..
 
 echo web_assembly ___________________________________________________________________________________________________________
