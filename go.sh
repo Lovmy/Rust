a86d1c982acdad6ff8b4707c29508479f756eb0d
BANASZAK Serge <serge.banaszak@eiffage.com>
2026-01-05 10:42:59 +0100

    Packages sous Ubuntu, Fedora et Alpine Linux

--- go.sh
+++ go.sh
@@ -2,16 +2,21 @@
 
 # Pour le statique :
 # 	rustup target add x86_64-unknown-linux-musl
-# 	sudo apt-get install -y musl-tools
+# 	sudo apt-get install -y musl-tools (Ubuntu)
+#	sudo dnf install -y musl-gcc musl-devel (Fedora)
 # 	cargo install cargo-vendor-openssl (si erreur utiliser plutot : openssl = { version = "*", features = ["vendored"] })
 
 # Pour le NAS :
 # 	rustup target add aarch64-unknown-linux-gnu
-# 	sudo apt-get install -y gcc-aarch64-linux-gnu pkg-config
+# 	sudo apt-get install -y gcc-aarch64-linux-gnu pkg-config (Ubuntu)
+#	sudo dnf install -y gcc-aarch64-linux-gnu pkgconf-pkg-config (Fedora)
+#	apk add --no-cache aarch64-linux-musl-gcc pkgconf (Alpine)
 
 # Pour Windows ( à partir d'un Linux )
 #	rustup target add x86_64-pc-windows-gnu
-#	sudo apt-get install mingw-w64
+#	sudo apt-get install mingw-w64 (Ubuntu)
+#	sudo dnf install -y mingw64-gcc mingw64-crt mingw64-headers (Fedora)
+#	apk add --no-cache mingw-w64-gcc (Alpine)
 #	Configurer le linker : créer/éditer ~/.cargo/config.toml :
 #	[target.x86_64-pc-windows-gnu]
 #	linker = "x86_64-w64-mingw32-gcc"
