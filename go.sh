dc3298ae8780466246c504101588a23e52d41b3f
BANASZAK Serge <serge.banaszak@eiffage.com>
2026-01-05 09:17:23 +0100

    Cross compilation Windows

--- go.sh
+++ go.sh
@@ -10,8 +10,11 @@
 # 	sudo apt-get install -y gcc-aarch64-linux-gnu pkg-config
 
 # Pour Windows ( à partir d'un Linux )
-#	rustup target add x86_64-pc-windows-msvcrustup target add x86_64-pc-windows-msvc
+#	rustup target add x86_64-pc-windows-gnu
 #	sudo apt-get install mingw-w64
+#	Configurer le linker : créer/éditer ~/.cargo/config.toml :
+#	[target.x86_64-pc-windows-gnu]
+#	linker = "x86_64-w64-mingw32-gcc"
 
 echo Mise a jour ____________________________________________________________________________________________________________
 cd ~/.cargo/bin
@@ -21,12 +24,12 @@ cargo install wasm-pack
 echo application ____________________________________________________________________________________________________________
 cd ~/workspace/Rust/application
 cargo clean
-# OPENSSL_STATIC=1 PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target x86_64-unknown-linux-musl			# Statique
+OPENSSL_STATIC=1 PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target x86_64-unknown-linux-musl			# Statique
 # CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc cargo build --target aarch64-unknown-linux-gnu	# NAS
-cargo build --release # --target x86_64-pc-windows-msvc									# Windows
+# cargo build --release --target x86_64-pc-windows-gnu									# Windows
 cd ..
 
-echo web_assembly ___________________________________________________________________________________________________________
+#echo web_assembly ___________________________________________________________________________________________________________
 cd ~/workspace/Rust/web_assembly
 cargo clean
 cargo build --target wasm32-unknown-unknown --release
